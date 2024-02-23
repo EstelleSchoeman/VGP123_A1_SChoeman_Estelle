b6e1f55306722d673d4dbb23866f65bf4e813ff5
EstelleSchoeman <estelle.schoeman@torontofilmschool.ca>
2024-02-23 17:41:41 -0500

    Camera corrected

--- Assets/Scripts/Camera follow.cs
+++ Assets/Scripts/Camera follow.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Camerafollow : MonoBehaviour
 {
     [SerializeField] Transform _target;
+
     [SerializeField] float minXClamp;
     [SerializeField] float maxXClamp;
-    [SerializeField] float minyClamp;
+    [SerializeField] float minYClamp;
     [SerializeField] float maxYClamp;
 
+
+    void FixedUpdate()
+    {
+       Vector3 cameraPos = transform.position;
+
+        cameraPos.x = Mathf.Clamp(_target.transform.position.x, minXClamp, maxXClamp);
+        cameraPos.y = Mathf.Clamp(_target.transform.position.y, minYClamp, maxYClamp);
+
+        transform.position = cameraPos;
+    }
 
     // Start is called before the first frame update
     void Start()
