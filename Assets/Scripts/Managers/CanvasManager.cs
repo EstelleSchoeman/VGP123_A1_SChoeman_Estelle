b47c95e0e94bde122f1a77cd3bd7eb44be8df12b
EstelleSchoeman <estelle.schoeman@torontofilmschool.ca>
2024-03-12 23:45:48 -0400

    PauseMenu buttons included

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -20,7 +21,7 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject pauseMenu;
 
     [Header("Text")]
-    public TMP_Text volSliderTExt;
+    public TMP_Text volSliderText;
     public TMP_Text livesText;
 
     [Header("Slider")]
@@ -30,8 +31,39 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (playButton)
+            playButton.onClick.AddListener(() => GameManager.Instance.ChangeScene(1));
+
+        if (settingsButton)
+            settingsButton.onClick.AddListener(() => SetMenus(settingMenu,mainMenu));
+
+        if (backButton)
+            backButton.onClick.AddListener(() => SetMenus(mainMenu, settingMenu));
         
+        if(volSlider)
+        {
+            volSlider.onValueChanged.AddListener(OnSliderValueChanged);
+            if(volSliderText)
+                volSliderText.text = volSlider.value.ToString();
+        }
+
     }
+    void OnSliderValueChanged(float value)
+    {
+        volSliderText.text = value.ToString();
+    }
+
+    void SetMenus(GameObject menuToActivate, GameObject menuToDeactivate)
+    {
+
+        if(menuToActivate)
+        menuToActivate.SetActive(true);
+
+        if(menuToDeactivate)
+       menuToDeactivate.SetActive(false);
+    }
+    
+   
 
     // Update is called once per frame
     void Update()
